2883efa3b851a0e02cf70b2a0fb1054dc504c2cb
abuzar <abuzardawar444@gmail.com>
2024-12-25 12:51:03 +0500

    `Update dependencies and configuration for Express server`

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-
-// middleware
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Server is listening on ${port}`));
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
