1ed5d938c9ccf8bc1723fd6312ff54f235c5322a
abuzar <abuzardawar444@gmail.com>
2024-12-30 13:03:20 +0500

    `Added updateUserPassword function and corresponding route to userController and userRoute respectively.`

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,3 +4,7 @@ import { StatusCodes } from "http-status-codes";
 export const showCurrentUser = async (req: Request, res: Response) => {
   res.status(StatusCodes.OK).json({ user: req.user });
 };
+
+export const updateUserPassword = async (req: Request, res: Response) => {
+  res.status(StatusCodes.OK).json({ message: "User password updated" });
+};
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { showCurrentUser } from "../controllers/userController";
+import {
+  showCurrentUser,
+  updateUserPassword,
+} from "../controllers/userController";
 import { authenticationMiddleware } from "../middleware/auth";
 
 const router = express.Router();
 
 router.route("/show-me").get(authenticationMiddleware, showCurrentUser);
-
+router.route("/password").patch(authenticationMiddleware, updateUserPassword);
 export default router;
